53fc5129515660a81f49770ffd015b678ba11108
[Naoya <[naoya1418@gmail.com]>
2023-06-22 14:09:07 +0900

    もろもろ修正

--- src/components/career.jsx
+++ src/components/career.jsx
@@ -8,9 +8,31 @@ import '../index.css';
 const Details = ({ positon, compony, companyLink, time, address, work }) => {
     const ref = useRef(null);
 
-    return <li ref={ ref } className='my-8 first:mt-0 last:mb-0 w-[60%] mx-auto flex flex-col items-start justify-between md:w-[80%]'>
+    return <li ref={ ref } className=' mx-auto flex justify-center p-4'>
+        <div className='w-1/3 p-4'>
+            <img src="./img/SIG_ACI.jpg" />
+        </div>
         <LiIcon/>
-        <motion.div
+
+            
+        
+        <div className='w-1/3 p-4'>
+        <h3 className='capitalize font-bold text-lg sm:text-xl lg:text-3xl'>{positon}&nbsp;<a href={companyLink}
+                target="_blank"
+                className='text-primary capitalize'
+            >@{compony}</a></h3>
+            <span className='capitalize font-medium text-sm lg:text-xl text-dark/75'>
+                {time} | {address}
+            </span>
+            <p className='font-medium w-full text-sm lg:text-xl'>
+                {work}
+            </p>
+
+
+
+        </div>
+
+        {/*<motion.div
             initial={{ y: 0 }}
             whileHover={{ y: -50 }}
             transition={{ duration: 0.5, type: "spring" }}
@@ -27,7 +49,7 @@ const Details = ({ positon, compony, companyLink, time, address, work }) => {
             <p className='font-medium w-full text-sm lg:text-xl'>
                 {work}
             </p>
-        </motion.div>
+        </motion.div>*/}
     </li>
 }
 
@@ -53,14 +75,8 @@ function Career() {
 
 
                     <ul className="w-full flex flex-col items-start justify-between ml-4 xs:ml-2">
-                        <div className='flex justify-center'>
-                        <motion.div
-                            initial={{ opacity: 0 }}
-                            whileInView={{ opacity: 1 }}
-                            viewport={{root:ref}}>
-                                
-                            <img src="./img/SIG_ACI.jpg" className='w-1/3' />
-                        </motion.div>
+                        
+
             
                         <Details
                             positon="アメリカ研修"
@@ -70,7 +86,7 @@ function Career() {
                             companyLink="https://missioncollege.edu/"
                             work="1か月間サンフランシスコに滞在。スタンフォードやAppleの見学や、当時行っていた研究を発表しました。"
                         />
-                        </div>
+                        
                         <Details
                             positon="滋賀県立彦根東高校 卒業"
                             compony="滋賀県"
--- src/components/contents/LiIcon.jsx
+++ src/components/contents/LiIcon.jsx
@@ -10,14 +10,10 @@ const LiIcon = ({reference}) => {
     )
 
     return (
-        <figure className="stroke-dark absolute left-0 dark:stroke-light">
+        <figure className="stroke-dark absolute  dark:stroke-light">
             <svg className="-rotate-90 w-[60px] h-[60px] xs:w-[40px] xs:h-[40px]" width="75" height="75" viewBox="0 0 100 100">
                 <circle cx="75" cy="50" r="20" className="stroke-primary stroke-1 fill-none"/>
-                <motion.circle cx="75" cy="50" r="20" className="stroke-[5px] fill-light"
-                    style={{
-                    pathLength : scrollYProgress
-                }}
-                />
+
                 <circle cx="75" cy="50" r="10" className="animate-pulse stroke-1 fill-primary"/>
             </svg>
         </figure>
